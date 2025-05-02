6ff944bd960097899bd533411581138e0ccd2513
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-05-02 23:04:02 +0530

    styled it better

--- customdropdown/trying/src/App.tsx
+++ customdropdown/trying/src/App.tsx
@@ -8,19 +8,19 @@ function App() {
   return (
     <>
     <div className='flex flex-col justify-center items-center min-h-screen'>
-      <button className='cursor-pointer bg-muted-foreground p-4 border-1 rounded-lg flex items-center gap-4' onClick={()=>{
-        setIsOpen(true);
+      <button className='relative cursor-pointer bg-slate-100 p-4 border-1 rounded-lg flex items-center gap-4 w-[150px]' onClick={()=>{
+        setIsOpen(!isOpen);
       }}>open dropdown <ArrowDown className='size-4'></ArrowDown></button>
       {isOpen ? (
-        <div className='flex'>
-          <ul>
-            <li>hi</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>dfg</li>
-            <li>gfhjf</li>
-            <li className='cursor-pointer' onClick={()=>{
+        <div>
+          <ul className='flex flex-col gap-2 mt-3 w-[150px] rounded-lg justify-center items-center bg-slate-100 border-1'>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>hi</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>asdf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>asdf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>asdf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>dfg</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>gfhjf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'onClick={()=>{
               setIsOpen(false);
             }}>close</li>
           </ul>
