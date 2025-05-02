0a168b9c8cc3a552b73d0111eaefc7355a1eb5b9
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-05-02 23:10:19 +0530

    further styling

--- customdropdown/trying/src/App.tsx
+++ customdropdown/trying/src/App.tsx
@@ -8,18 +8,18 @@ function App() {
   return (
     <>
     <div className='flex flex-col justify-center items-center min-h-screen'>
-      <button className='relative cursor-pointer bg-slate-100 p-4 border-1 rounded-lg flex items-center gap-4 w-[150px]' onClick={()=>{
+      <button className='fixed cursor-pointer bg-slate-100 p-4 border-1 rounded-lg flex items-center gap-4 w-[150px]' onClick={()=>{
         setIsOpen(!isOpen);
       }}>open dropdown <ArrowDown className='size-4'></ArrowDown></button>
       {isOpen ? (
-        <div>
-          <ul className='flex flex-col gap-2 mt-3 w-[150px] rounded-lg justify-center items-center bg-slate-100 border-1'>
-            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>hi</li>
-            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>asdf</li>
-            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>asdf</li>
-            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>asdf</li>
-            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>dfg</li>
-            <li className='border-b-1 w-full flex justify-center rounded-md p-4'>gfhjf</li>
+        <div className='transition-initial duration-700 translate-y-1/12 '>
+          <ul className='flex flex-col gap-2 mt-3 w-[150px] rounded-lg justify-center items-center bg-slate-100 border-1 '>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'>hi</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'>asdf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'>asdf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'>asdf</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'>dfg</li>
+            <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'>gfhjf</li>
             <li className='border-b-1 w-full flex justify-center rounded-md p-4 cursor-pointer'onClick={()=>{
               setIsOpen(false);
             }}>close</li>
